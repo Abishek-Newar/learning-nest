72528a4177950e9ce868df956c0b3c2b55c59197
Abishek Newar <abisheknewar15@gmail.com>
2025-11-28 12:37:49 +0530

    database integration completed
    integrated prisma ORM and psotgres databse that i s hosted on docker

---
+++ src/database/database.service.ts
@@ -0,0 +1,17 @@
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from '@prisma/client';
+import { Pool } from 'pg';
+@Injectable()
+export class DatabaseService extends PrismaClient implements OnModuleInit {
+    constructor(){
+        const connectionString:string = process.env.DATABASE_URL as string;
+        const pool = new Pool({ connectionString })
+
+        const adapter = new PrismaPg(pool);
+        super({adapter});
+    }
+    onModuleInit() {
+        this.$connect()
+    }
+}
