08f109b5a3c66c6ce5bfc1340835e21690c3b593
Abishek Newar <abisheknewar15@gmail.com>
2025-11-28 11:38:00 +0530

    first commit

---
+++ prisma/migrations/20251128055653_first_migration/migration.sql
@@ -0,0 +1,14 @@
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('ADMIN', 'INTERN', 'ENGINEER');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "role" "Role" NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
